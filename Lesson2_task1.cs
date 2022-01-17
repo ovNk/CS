d97d8ac228d1820e2414f90a48e71876a0849acb
ovNk <97682911+ovNk@users.noreply.github.com>
2022-01-17 18:05:43 +0300

    Add files via upload

---
+++ Lesson2_task1.cs
@@ -0,0 +1,32 @@
+﻿/*
+Ввести с клавиатуры три названия городов.
+Вывести на печать их через запятую, в конце поставить точку.
+ */
+using System;
+
+namespace Lesson2_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city_1;
+            string city_2;
+            string city_3;
+
+            //Ввод городов
+            Console.WriteLine("City N1: ");
+            city_1 = Console.ReadLine();
+            Console.WriteLine("City N2: ");
+            city_2 = Console.ReadLine();
+            Console.WriteLine("City N3: ");
+            city_3 = Console.ReadLine();
+
+            //Вывод через , с . на конце
+            Console.WriteLine($"{city_1}, {city_2}, {city_3}.");
+
+            return;
+
+        }
+    }
+}
---
+++ Lesson2_task2.cs
@@ -0,0 +1,33 @@
+﻿/*
+Написать программу, которая считывает символы
+с клавиатуры, пока не будет введена точка.
+Программа должна сосчитать количество введенных
+пользователем пробелов.
+*/
+using System;
+
+namespace Lesson2_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch;//символ
+            int space = 0;//переменная для подсчета количества пробелов
+
+            Console.WriteLine("Characters: ");
+            ch = (char)Console.Read();//читаем первый символ
+
+            while (ch != 46) //пока не введена .
+            {
+                if (ch == 32)//если введен пробел
+                    ++space;
+                ch = (char)Console.Read();//читаем следующий символ
+            }
+
+            Console.WriteLine($"Spaces: {space}.");//выводим количество пробелов
+
+            return;
+        }
+    }
+}
