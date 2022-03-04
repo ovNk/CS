808626d1ab5dc62c73b6e913575a777f26aca8a4
ovNk <97682911+ovNk@users.noreply.github.com>
2022-03-04 21:07:45 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,36 +12,7 @@ class Program
 
         static void Main(string[] args)
         {
-            //XmlDocument xDoc = new XmlDocument();
-            //xDoc.Load(@"D:\Users\oniki\Desktop\Тест\LessonXML.xml");
-            //// получим корневой элемент
-            //XmlElement? xRoot = xDoc.DocumentElement;
-            //if (xRoot != null)
-            //{
-            //    // обход всех узлов в корневом элементе
-            //    foreach (XmlElement xnode in xRoot)
-            //    {
-            //        // получаем атрибут name
-            //        XmlNode? attr = xnode.Attributes.GetNamedItem("name");
-            //        Console.WriteLine(attr?.Value);
-
-            //        // обходим все дочерние узлы элемента user
-            //        foreach (XmlNode childnode in xnode.ChildNodes)
-            //        {
-            //            // если узел - company
-            //            if (childnode.Name == "company")
-            //            {
-            //                Console.WriteLine($"Company: {childnode.InnerText}");
-            //            }
-            //            // если узел age
-            //            if (childnode.Name == "age")
-            //            {
-            //                Console.WriteLine($"Age: {childnode.InnerText}");
-            //            }
-            //        }
-            //        Console.WriteLine();
-            //    }
-            //}
+            
             IWeapon bow = new Bow();
             //bow.Sharpen();
             IWeapon[] weapons = new IWeapon[] { new Bow(), new Gun(), new Knife() };
